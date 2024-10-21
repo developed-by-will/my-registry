1d0cec19e4d2b5919ea08b11275e464ecb9b1429
developerwill <info@wilsongomes.me>
2024-10-21 17:23:10 +0000

    Homepage setup

---
+++ app/(pages)/Homepage/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col justify-center px-4 gap-8 mt-4 lg:mt-8 mb-16">
+      <header className="text-center">
+        <h1 className="text-sm sm:text-2xl">Component Registry</h1>
+        <h2 className="text-xl sm:text-2xl font-semibold text-blue-500">
+          Hello world!
+        </h2>
+      </header>
+    </div>
+  );
+}
---
+++ app/commonSEO.ts
@@ -0,0 +1,30 @@
+export const commonSEO = {
+    authors: [{ name: "developerwill" }],
+    keywords: [
+      "ui",
+      "Next.js",
+      "React.js",
+      "NextJs",
+      "React",
+      "Tailwind CSS",
+      "Server Components",
+      "Radix UI",
+    ],
+    creator: "developerwill",
+  };
+
+export const commonOpenGraph = {
+    url: "https://component-registry.wilsongomes.me",
+    siteName: "component-registry",
+    locale: "en_US",
+    images: [
+      {
+        url: "https://component-registry/og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "component-registry",
+      },
+    ],
+    type: "website",
+    countryName: "Portugal",
+  };
